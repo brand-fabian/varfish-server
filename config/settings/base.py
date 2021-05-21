ffffe847f38bdfeec143eb61bd6ab775c6f43873
Fabian Brand <brand@imbie.meb.uni-bonn.de>
2021-05-21 10:25:15 +0200

    SAML config update.

--- config/settings/base.py
+++ config/settings/base.py
@@ -744,19 +744,19 @@ SAML2_AUTH = {
         ],
     },
     'DEFAULT_NEXT_URL': '/',  # Custom target redirect URL after the user get logged in. Default to /admin if not set. This setting will be overwritten if you have parameter ?next= specificed in the login URL.
-    # # Optional settings below
-    # 'NEW_USER_PROFILE': {
-    #     'USER_GROUPS': [],  # The default group name when a new user logs in
-    #     'ACTIVE_STATUS': True,  # The default active status for new users
-    #     'STAFF_STATUS': True,  # The staff status for new users
-    #     'SUPERUSER_STATUS': False,  # The superuser status for new users
-    # },
-    # 'ATTRIBUTES_MAP': {  # Change Email/UserName/FirstName/LastName to corresponding SAML2 userprofile attributes.
-    #     'email': 'Email',
-    #     'username': 'UserName',
-    #     'first_name': 'FirstName',
-    #     'last_name': 'LastName',
-    # },
+    # Optional settings below
+    'NEW_USER_PROFILE': {
+        'USER_GROUPS': [],  # The default group name when a new user logs in
+        'ACTIVE_STATUS': True,  # The default active status for new users
+        'STAFF_STATUS': True,  # The staff status for new users
+        'SUPERUSER_STATUS': False,  # The superuser status for new users
+    },
+    'ATTRIBUTES_MAP': {  # Change Email/UserName/FirstName/LastName to corresponding SAML2 userprofile attributes.
+        'email': 'urn:oid:1.2.840.113549.1.9.1',
+        'username': 'username',
+        'first_name': 'urn:oid:2.5.4.42',
+        'last_name': 'urn:oid:2.5.4.4',
+    },
     # 'TRIGGER': {
     #     'FIND_USER': 'path.to.your.find.user.hook.method',
     #     'NEW_USER': 'path.to.your.new.user.hook.method',
